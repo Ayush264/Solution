25ffd01584e643aeec89d83c5df2a4ed426bba6a
Ayush264 <121258575+Ayush264@users.noreply.github.com>
2024-08-22 10:43:36 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 15;
+        int num2 = 5;
+        
+        // Calculate the difference
+        int difference = num1 - num2;
+        
+        // Print the result
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+    }
+}
